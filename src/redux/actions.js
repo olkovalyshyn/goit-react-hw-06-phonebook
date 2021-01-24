3d4c204e40ef19efad4cd3913d6dade1f3406da7
Olexandr Kovalyshyn <kovalyshyn@gmail.com>
2021-01-24 02:29:51 +0200

    Add on redux toolkit

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,25 +1,35 @@
+import { createAction } from "@reduxjs/toolkit";
 import shortid from "shortid";
-import { ADDCONTACT, DELCONTACT, FINDCONTACT } from "./contact-types";
+// import { ADDCONTACT, DELCONTACT, FINDCONTACT } from "./contact-types";
 
-export const addContact = (name, number) => ({
-  type: ADDCONTACT,
-  payload: {
-    id: shortid.generate(),
-    name: name,
-    number: number,
-  },
+export const addContact = createAction("Contact/addContact", (name, number) => {
+  return {
+    payload: {
+      id: shortid.generate(),
+      name: name,
+      number: number,
+    },
+  };
 });
+// export const addContact = (name, number) => ({
+//   type: ADDCONTACT,
+//   payload: {
+//     id: shortid.generate(),
+//     name: name,
+//     number: number,
+//   },
+// });
 
-console.log("!!!addContact", addContact());
+export const deleteContact = createAction("Contact/deleteContact");
+// export const deleteContact = (idForDelete) => ({
+//   type: DELCONTACT,
+//   payload: idForDelete,
+// });
 
-export const deleteContact = (idForDelete) => ({
-  type: DELCONTACT,
-  payload: idForDelete,
-});
-
-export const filterContact = (value) => ({
-  type: FINDCONTACT,
-  payload: value,
-});
+export const filterContact = createAction("Contact/ChangeFilter");
+// export const filterContact = (value) => ({
+//   type: FINDCONTACT,
+//   payload: value,
+// });
 
 export default { addContact, deleteContact, filterContact };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,12 @@
 import { createStore, combineReducers } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { configureStore } from "@reduxjs/toolkit";
+import { createReducer } from "@reduxjs/toolkit";
+// import { composeWithDevTools } from "redux-devtools-extension";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-import { ADDCONTACT, DELCONTACT, FINDCONTACT } from "./contact-types";
+import actions from "./actions";
+// import { ADDCONTACT, DELCONTACT, FINDCONTACT } from "./contact-types";
 
 const contacts = {
   items: [
@@ -13,66 +18,48 @@ const contacts = {
   filter: "",
 };
 
-console.log("!!!in Store contacts.items", contacts.items);
-console.log("!!!in Store contacts.filter", contacts.filter);
-console.log("state = contacts.items", contacts.items);
+console.log("!!!actions.addContact", actions.addContact);
+console.log("!!!actions.addContact.type", actions.addContact.type);
 
-// const initialState = [
-//   { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-//   { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-//   { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-//   { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-// ];
-
-// const reducer = (state = initialState.contacts.items, { type, payload }) => {
+const itemReducer = createReducer(contacts.items, {
+  [actions.addContact]: (state, { payload }) => [payload, ...state],
+  [actions.deleteContact]: (state, { payload }) =>
+    state.filter(({ id }) => id !== payload),
+});
+// const itemReducer = (state = contacts.items, { type, payload }) => {
 //   switch (type) {
 //     case ADDCONTACT:
 //       return [payload, ...state];
-
 //     case DELCONTACT:
 //       return state.filter(({ id }) => id !== payload);
+//     default:
+//       return state;
+//   }
+// };
+
+const filterReducer = createReducer(contacts.filter, {
+  [actions.filterContact]: (_, { payload }) => payload,
+});
+// const filterReducer = (state = contacts.filter, { type, payload }) => {
+//   switch (type) {
+//     case FINDCONTACT:
+//       return payload;
 
 //     default:
 //       return state;
 //   }
 // };
 
-const itemReducer = (state = contacts.items, { type, payload }) => {
-  switch (type) {
-    case ADDCONTACT:
-      return [payload, ...state];
-
-    case DELCONTACT:
-      return state.filter(({ id }) => id !== payload);
-
-    default:
-      return state;
-  }
-};
-
-const filterReducer = (state = contacts.filter, { type, payload }) => {
-  switch (type) {
-    case FINDCONTACT:
-      return payload;
-
-    default:
-      return state;
-  }
-};
-
 const contactsReducer = combineReducers({
   item: itemReducer,
   filter: filterReducer,
 });
 
-const rootReducer = combineReducers({
-  contacts: contactsReducer,
+const store = configureStore({
+  reducer: {
+    contacts: contactsReducer,
+  },
+  devTools: process.env.NODE_ENV === "development",
 });
-
-const store = createStore(
-  rootReducer,
-  composeWithDevTools()
-  // other store enhancers if any
-);
 
 export default store;
